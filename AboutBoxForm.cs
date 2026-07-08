17c774ba12a961b4886603bab5b2e189b89d37d9
Michael de Radobýl <mj-programming@gmx.de>
2026-07-08 10:19:18 +0200

    Refactor `AboutBoxForm` with consistent formatting

--- AboutBoxForm.cs
+++ AboutBoxForm.cs
@@ -3,27 +3,19 @@ using NLog;
 
 namespace Numeric_List_Generator
 {
-	/// <summary>
-	/// Represents the About Box form of the application.
-	/// </summary>
+	/// <summary>Represents the About Box form of the application.</summary>
 	[DebuggerDisplay(value: $"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 
 	internal sealed partial class AboutBoxForm : Form
 	{
-		/// <summary>
-		/// Logger instance for logging messages and exceptions.
-		/// </summary>
+		/// <summary>Logger instance for logging messages and exceptions.</summary>
 		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-		/// <summary>
-		/// Returns a string that represents the current object for debugging purposes.
-		/// </summary>
+		/// <summary>Returns a string that represents the current object for debugging purposes.</summary>
 		/// <returns>A string that represents the current object.</returns>
 		private string GetDebuggerDisplay() => ToString();
 
-		/// <summary>
-		/// Sets a specific text to the status bar.
-		/// </summary>
+		/// <summary>Sets a specific text to the status bar.</summary>
 		/// <param name="text">The text with some information to display in the status bar.</param>
 		private void SetStatusBarText(string text)
 		{
@@ -31,10 +23,8 @@ namespace Numeric_List_Generator
 			labelInformation.Text = text;
 		}
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="AboutBoxForm"/> class.
-		/// Sets the text of the labels to the corresponding assembly information.
-		/// </summary>
+		/// <summary>Initializes a new instance of the <see cref="AboutBoxForm"/> class.
+		/// Sets the text of the labels to the corresponding assembly information.</summary>
 		public AboutBoxForm()
 		{
 			InitializeComponent();
@@ -49,17 +39,13 @@ namespace Numeric_List_Generator
 			KeyPreview = true; // Ensures the form receives key events before the controls
 		}
 
-		/// <summary>
-		/// Handles the Load event of the AboutBoxForm control.
-		/// Initializes the status bar text to an empty string.
-		/// </summary>
+		/// <summary>Handles the Load event of the AboutBoxForm control.
+		/// Initializes the status bar text to an empty string.</summary>
 		/// <param name="sender">The source of the event.</param>
 		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
 		private void AboutBoxForm_Load(object sender, EventArgs e) => SetStatusBarText(text: string.Empty);
 
-		/// <summary>
-		/// Called when the mouse pointer moves over a control.
-		/// </summary>
+		/// <summary>Called when the mouse pointer moves over a control.</summary>
 		/// <param name="sender">The event source.</param>
 		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
 		private void SetStatusBar_Enter(object sender, EventArgs e)
@@ -78,9 +64,7 @@ namespace Numeric_List_Generator
 			}
 		}
 
-		/// <summary>
-		/// Clears the information text of the status bar.
-		/// </summary>
+		/// <summary>Clears the information text of the status bar.</summary>
 		/// <param name="sender">The source of the event.</param>
 		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
 		private void ClearStatusBar_Leave(object sender, EventArgs e) => SetStatusBarText(text: string.Empty);
@@ -116,10 +100,8 @@ namespace Numeric_List_Generator
 			}
 		}
 
-		/// <summary>
-		/// Handles the KeyDown event of the ExportDataSheetForm.
-		/// Closes the form when the Escape key is pressed.
-		/// </summary>
+		/// <summary>Handles the KeyDown event of the ExportDataSheetForm.
+		/// Closes the form when the Escape key is pressed.</summary>
 		/// <param name="sender">The event source.</param>
 		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
 		private void AboutBoxForm_KeyDown(object? sender, KeyEventArgs e)
