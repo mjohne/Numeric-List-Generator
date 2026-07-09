cbb4a29ae8d178900905ffbfe43b8dca769c723b
Michael de Radobýl <mj-programming@gmx.de>
2026-07-09 09:33:31 +0200

    Refactor `LicenseForm.cs` by removing unnecessary lines

--- LicenseForm.cs
+++ LicenseForm.cs
@@ -33,7 +33,6 @@ namespace Numeric_List_Generator
 		}
 
 		/// <summary>Handles the Load event of the <see cref="LicenseForm"/>.</summary>
-
 		/// <param name="sender">object sender</param>
 		/// <param name="e">event arguments</param>
 		/// <remarks>The parameters <paramref name="e"/> and <paramref name="sender"/> are not needed, but must be indicated.</remarks>
@@ -62,8 +61,8 @@ namespace Numeric_List_Generator
 		/// <param name="sender">The source of the event.</param>
 		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
 		private void ClearStatusBar_Leave(object sender, EventArgs e) => SetStatusBar(text: string.Empty);
-		/// <summary>Handles the KeyDown event of the <see cref="LicenseForm"/>. Closes the form when the Escape key is pressed.</summary>
-		/// Closes the form when the Escape key is pressed.</summary>
+
+		/// <summary>Handles the KeyDown event of the <see cref="LicenseForm"/>. Closes the form when the Escape key is pressed.</summary>
 		/// <param name="sender">The event source.</param>
 		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
 		private void LicenseForm_KeyDown(object? sender, KeyEventArgs e)
